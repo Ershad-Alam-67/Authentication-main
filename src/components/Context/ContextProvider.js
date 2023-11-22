a3ce449c5bc5e534ea177415a788663d35b5f7d2
Ershad-Alam-67 <ealam6732@gmail.com>
2023-11-22 16:47:24 +0530

    Auto logout

--- src/components/Context/ContextProvider.js
+++ src/components/Context/ContextProvider.js
@@ -6,15 +6,31 @@ const ContextProvider = ({ children }) => {
   const [token, setToken] = useState(storedUser.token || "")
   const [isLogIn, setIsLogIn] = useState(storedUser.isLogIn || false)
 
+  const handleLogout = () => {
+    localStorage.removeItem("User")
+    setToken("")
+    setIsLogIn(false)
+  }
+
   useEffect(() => {
     localStorage.setItem("User", JSON.stringify({ token, isLogIn }))
   }, [token, isLogIn])
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      handleLogout()
+      alert("Your session has expired. Please log in again.")
+    }, 5 * 60 * 1000)
+
+    return () => clearTimeout(timeoutId)
+  }, [token])
 
   const contextValue = {
     token,
     setToken,
     isLogIn,
     setIsLogIn,
+    handleLogout,
   }
 
   return (
